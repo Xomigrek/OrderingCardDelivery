1f395e65a5670b625b84f32c56aeff10aeed5b8b
Igor Khomenko <xomigrek@gmail.com>
2022-09-12 21:39:53 +0300

    Changed method of setting data, Changed name of package

--- src/test/java/ru/netology/оrderingсarddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/оrderingсarddelivery/CardDeliveryTest.java
@@ -1,4 +1,4 @@
-package ru.netology.OrderingCardDelivery;
+package ru.netology.оrderingсarddelivery;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
@@ -8,6 +8,8 @@ import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -15,29 +17,32 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardDeliveryTest {
-    public String setCalendar() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 4);
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
-        String str = format1.format(calendar.getTime());
-        return str;
+//    public String setCalendar(int days) {
+//        Calendar calendar = new GregorianCalendar();
+//        calendar.add(Calendar.DAY_OF_YEAR, days);
+//        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+//        String str = format1.format(calendar.getTime());
+//        return str;
+//    }
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void shouldRegisterOrder () {
-        Configuration.holdBrowserOpen=true;
         open("http://localhost:9999/");
+        String planningDate = generateDate(4);
         $$("[type=\"text\"]").first().setValue("Санкт-Петербург");
         $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.CONTROL + "A");
         $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
-        $x("//input[@placeholder=\"Дата встречи\"]").setValue(setCalendar());
+        $x("//input[@placeholder=\"Дата встречи\"]").setValue(planningDate);
         $$("[type=\"text\"]").last().setValue("Хоменко Игорь");
         $x("//input[@name=\"phone\"]").setValue("+79119994143");
         $(".checkbox").click();
         $("button.button").click();
         $x("//div[@class=\"notification__content\"]").should(Condition.visible, Duration.ofSeconds(15));
         String text = $x("//div[@class=\"notification__content\"]").getText();
-        assertEquals("Встреча успешно забронирована на " + setCalendar(),text.trim());
+        assertEquals("Встреча успешно забронирована на " + planningDate,text.trim());
 
     }
 }
